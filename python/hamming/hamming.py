abfe3ae6c384ca8b70785797e407864cc702b94f
Kristian Bonitz <kristian@bonitz.me>
2019-06-13 07:26:31 +1000

    First Iteration of HammingDistance
    Simple iteration through 2 lists.

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,11 @@
 def distance(strand_a, strand_b):
-    pass
+	if len(strand_a) != len(strand_b):
+		raise ValueError("strands are different lengths")
+
+	hamming_distance = 0
+
+	for x in range(0, len(strand_a)):
+		if strand_a[x] != strand_b[x]:
+			hamming_distance += 1
+
+	return hamming_distance
