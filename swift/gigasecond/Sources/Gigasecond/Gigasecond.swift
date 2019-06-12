80b5336bbc590186c003383ac009a03760342db8
Kristian Bonitz <kristian@bonitz.me>
2019-06-13 07:12:09 +1000

    Completing Giga-second Challenge
    Didn't realise that I needed to set a timezone for the date-formatter to work correctly. After that everything worked okay.

--- swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
+++ swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
@@ -5,17 +5,20 @@ class Gigasecond {
     let from: Date
     let description: String
     
-    init(from f: String){
+    init?(from f: String){
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
         
-        self.from = dateFormatter.date(from: f)!
+        self.from = dateFormatter.date(from: f) ?? Date()
         
         self.description =
             dateFormatter.string(
                 from: Date.init(
                     timeInterval: 1000000000,
                     since: self.from
-        ))
+                )
+            )
     }
 }
+
