8a193e63bcda684b2b0cb27442f999b1edda5ba0
Kristian Bonitz <kristian@bonitz.me>
2019-06-13 07:38:26 +1000

    Hamming Distance Iteration 2
    Using List Constructors, reduced the length of the script down. Keeping the speed and readability.

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,10 +2,6 @@ def distance(strand_a, strand_b):
 	if len(strand_a) != len(strand_b):
 		raise ValueError("strands are different lengths")
 
-	hamming_distance = 0
+	hamming = [x for x in range(0, len(strand_a)) if strand_a[x] != strand_b[x]]
 
-	for x in range(0, len(strand_a)):
-		if strand_a[x] != strand_b[x]:
-			hamming_distance += 1
-
-	return hamming_distance
+	return len(hamming)
