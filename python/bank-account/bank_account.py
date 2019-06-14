42a56a178fc78508d5584584734f080d81172577
Kristian Bonitz <kristian@bonitz.me>
2019-06-14 10:02:53 +1000

    First iteration of bank_account
    Learning how to handle multiple threads is interesting. Second run will clean it up to handle everything more elegently.

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,25 +1,49 @@
+import threading
+
 class BankAccount(object):
     def __init__(self):
-        self.is_open = None
+        self.is_open = False
         self._balance = 0
+        self.lock = threading.Lock()
 
     def get_balance(self):
-        return self._balance
+        self.lock.acquire()
+        if self.is_open:
+            return self._balance
+        else:
+            raise ValueError("Cannot get balance of closed account")
+        self.lock.release()
 
     def open(self):
-        if self.is_open != None:
+        self.lock.acquire()
+        if self.is_open != False:
             raise ValueError("Cannot open account with status", self.is_open)
 
         self.is_open = True
+        self.lock.release()
 
     def deposit(self, amount):
-        if self.is_open:
+        self.lock.acquire()
+        if self.is_open and amount > 0:
             self._balance += amount
         else:
-            raise ValueError("Cannot into account")
+            raise ValueError("Cannot deposit into account")
+        self.lock.release()
 
     def withdraw(self, amount):
-        pass
+        self.lock.acquire()
+        if self.is_open and amount <= self._balance and amount > 0:
+            self._balance -= amount
+        else:
+            raise ValueError("Invalid operation")
+        self.lock.release()
 
     def close(self):
-        pass
+        self.lock.acquire()
+        if self.is_open:
+            self.is_open = False
+            self._balance = 0
+        else:
+            raise ValueError("Cannot close already closed account")
+        self.lock.release()
+        
