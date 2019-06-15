68a67a7c3924116e9b53d8e0c821678a17576a52
Kristian Bonitz <kristian@bonitz.me>
2019-06-15 14:37:23 +1000

    Second iteration of bank-account
    changed the two aquire/release statements into a single `with lock` statement

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -4,46 +4,40 @@ class BankAccount(object):
     def __init__(self):
         self.is_open = False
         self._balance = 0
-        self.lock = threading.Lock()
+        self._lock = threading.Lock()
 
     def get_balance(self):
-        self.lock.acquire()
-        if self.is_open:
-            return self._balance
-        else:
-            raise ValueError("Cannot get balance of closed account")
-        self.lock.release()
+        with self._lock:
+            if self.is_open:
+                return self._balance
+            else:
+                raise ValueError("Cannot get balance of closed account")
 
     def open(self):
-        self.lock.acquire()
-        if self.is_open != False:
-            raise ValueError("Cannot open account with status", self.is_open)
+        with self._lock:
+            if self.is_open != False:
+                raise ValueError("Cannot open account with status", self.is_open)
 
-        self.is_open = True
-        self.lock.release()
+            self.is_open = True
 
     def deposit(self, amount):
-        self.lock.acquire()
-        if self.is_open and amount > 0:
-            self._balance += amount
-        else:
-            raise ValueError("Cannot deposit into account")
-        self.lock.release()
+        with self._lock:
+            if self.is_open and amount > 0:
+                self._balance += amount
+            else:
+                raise ValueError("Cannot deposit into account")
 
     def withdraw(self, amount):
-        self.lock.acquire()
-        if self.is_open and amount <= self._balance and amount > 0:
-            self._balance -= amount
-        else:
-            raise ValueError("Invalid operation")
-        self.lock.release()
+        with self._lock:
+            if self.is_open and amount <= self._balance and amount > 0:
+                self._balance -= amount
+            else:
+                raise ValueError("Invalid operation")
 
     def close(self):
-        self.lock.acquire()
-        if self.is_open:
-            self.is_open = False
-            self._balance = 0
-        else:
-            raise ValueError("Cannot close already closed account")
-        self.lock.release()
-        
+        with self._lock:
+            if self.is_open:
+                self.is_open = False
+                self._balance = 0
+            else:
+                raise ValueError("Cannot close already closed account")        
