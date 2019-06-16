f4af1dfe7d6d63499bd473061c973e151b1b6049
Kristian Bonitz <kristian@bonitz.me>
2019-06-17 09:14:56 +1000

    Iso-grams first iteration
    First run at the iso-grams problem. Pretty simple and clear solution. Required a bit more knowledge of the python string functions.

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,6 +1,12 @@
 def is_isogram(string):
-	for char_a in string:
-		for char_b in string:
-			if char_a == char_b:
-				return False
-	return True
+    used_letters = []
+
+    for char in string.lower():
+    	if char.isalpha() == False:
+    		pass
+    	elif char in used_letters:
+    		return False
+    	else:
+    		used_letters.append(char)
+
+    return True
