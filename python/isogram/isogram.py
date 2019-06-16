50ae490a9f635c5c945a7c952be27a67a4c812f5
Kristian Bonitz <kristian@bonitz.me>
2019-06-17 09:56:57 +1000

    Isograms second iteration
    Fixing some slight formatting to make the solution more simple and straight forward.

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,12 +1,6 @@
 def is_isogram(string):
-    used_letters = []
-
     for char in string.lower():
-    	if char.isalpha() == False:
-    		pass
-    	elif char in used_letters:
+    	if char.isalpha() and string.lower().count(char) > 1:
     		return False
-    	else:
-    		used_letters.append(char)
 
     return True
