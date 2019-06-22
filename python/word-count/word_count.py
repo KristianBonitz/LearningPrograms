2b84d6325c0156592de822d690b1b44583758cb0
Kristian Bonitz <kristian@bonitz.me>
2019-06-22 11:34:36 +1000

    First iteration of word count problem
    Doesn't handle apostraphies correctly yet, but still serviceable in every other way.

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,10 +1,13 @@
+import re
+
 def count_words(sentence):
 	#format sentence to remove non-words and seperate words
-
-    words = sentence.split(' ')
-    word_count = {}
-    #count the number of words in sentence
-    for word in words:
-    	word_count[word] = sentence.count(word)
-    
-    return word_count
+	words = re.findall(r"(?<!\\)[a-z0-9]+", sentence.lower())
+	print(words)
+	#words = sentence.lower().split(r' ')
+	#count the number of words in sentence
+	word_count = {}
+	for word in words:
+		word_count[word] = words.count(word)
+	
+	return word_count
