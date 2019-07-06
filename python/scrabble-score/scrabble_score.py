1ec9a676a3adeea7c796391b74f770ded9d2189e
Kristian Bonitz <kristian@bonitz.me>
2019-07-06 12:30:19 +1000

    first iteration of Scrabble
    Completed, now to work on having a fluid system for scrabble.

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,19 @@
 def score(word):
-    pass
+	score = 0
+	for letter in word.upper():
+		if letter in ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']:
+			score += 1
+		elif letter in ['D', 'G']:
+			score += 2
+		elif letter in ['B', 'C', 'M', 'P']:
+			score += 3
+		elif letter in ['F', 'H', 'V', 'W', 'Y']:
+			score += 4
+		elif letter in ['K']:
+			score += 5
+		elif letter in ['J', 'X']:
+			score += 8
+		elif letter in ['Z', 'Q']:
+			score += 10
+
+	return score
