29d2ff8832dca7e5e76cb163a7d1aaf9776ba635
Kristian Bonitz <kristian@bonitz.me>
2019-07-07 13:04:03 +1000

    Book: 1.6 Higher-order programming exerise
    Learning how to use lambda, maps and recursion all at once. Very fun and interesting.

--- Concepts, Techniques, and Models of Computer Programming - Notes/exercises_ch1.py
+++ Concepts, Techniques, and Models of Computer Programming - Notes/exercises_ch1.py
@@ -12,44 +12,44 @@ will be done in Python.
 import timeit
 start = timeit.timeit()
 
-# # Exercise 1.1a - Calculator 2^100
+# Exercise 1.1a - Calculator 2^100
 
-# v = 2*2*2*2*2
-# w = v*v*v*v*v
-# print(w*w*w*w)
+v = 2*2*2*2*2
+w = v*v*v*v*v
+x = w*w*w*w
 
-# # Exercise 1.1b - Calculator 100!
+# Exercise 1.1b - Calculator 100!
 
-# # with function
-# def bang(i):
-#     if i == 1:
-#         return i
-#     else:
-#         return i*bang(i-1)
+# with function
+def bang(i):
+    if i == 1:
+        return i
+    else:
+        return i*bang(i-1)
 
 # print(bang(100))
 
 
 # # Exercise 1.2 - Better/Smarting factorial calculations
 
-# def fact(N):
-#     if N == 0: 
-#         return 1
-#     else:
-#         return N * fact(N-1)
+def fact(N):
+    if N == 0: 
+        return 1
+    else:
+        return N * fact(N-1)
 
 
-# def fact_limit(N, K):
-#     if N==K:
-#         return 1 
-#     else: 
-#         return N * fact_limit(N-1, K)
+def fact_limit(N, K):
+    if N==K:
+        return 1 
+    else: 
+        return N * fact_limit(N-1, K)
 
-# def comb(N, K):
-#     return fact(N) / (fact(K) * fact(N - K))
+def comb(N, K):
+    return fact(N) / (fact(K) * fact(N - K))
 
-# def fast_comb(N, K):
-#     return fact_limit(N, N-K) / fact(K)
+def fast_comb(N, K):
+    return fact_limit(N, N-K) / fact(K)
 
 # start = timeit.timeit()
 # print (comb(500, 200))
@@ -65,20 +65,54 @@ start = timeit.timeit()
 # # Mozart handles this much more elegently than python, so this is a bit of an experiment
 # # getting to perform similarly.
 
-# from itertools import islice
+from itertools import islice
 
-# #infinite lazy list
-# def gen_ints(N=0):
-#     while True:
-#         yield N
-#         N += 1 
+#infinite lazy list
+def gen_ints(N=0):
+    while True:
+        yield N
+        N += 1 
 
-# def sum_list(it_list):
-#     return sum(it_list)
+def sum_list(it_list):
+    return sum(it_list)
 
-# #creates infinite loop 
+# creates infinite loop 
 # print(sum_list(gen_ints()))
 
 # Exercise 1.6 - Higher order programming
+def pascal(n):
+    if n <= 1:
+        return [1]
+    else:
+        row = pascal(n-1)
+        return op(shift_left(row), shift_right(row))
 
+def op(row_1, row_2):
+    return [row_1[x] + row_2[x] for x in range(0,len(row_1))]
 
+def shift_left(row):
+    return [0] + row
+
+def shift_right(row):
+    return row + [0]
+
+#print(pascal(25))
+
+def generic_pascal(operation, n):
+    if n == 1:
+        return [1]
+    else:
+        row = generic_pascal(operation, n-1)
+        print(row)
+        return op_list(operation, shift_left(row), shift_right(row))
+
+def op_list(operation, row_left, row_right):
+    return map(operation, row_left, row_right)
+
+adding = lambda a, b: a + b 
+multiply = lambda a, b: (a+1) * (b+1)
+doubling = lambda a, b: (a + b) * 2
+
+print(generic_pascal(adding, 11))
+
+print(generic_pascal(doubling, 10))
