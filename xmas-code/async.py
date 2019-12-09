b897603152ee6245ebdbcc8a78fd4510f76bdf7d
Kristian Bonitz <kristian@bonitz.me>
2019-12-10 07:46:48 +1100

    Day 9 + an async experiment

---
+++ xmas-code/async.py
@@ -0,0 +1,11 @@
+## the engine does something based on the inital setup
+## it awaits input
+## it produces output
+
+def engine(threshold, operation):
+    
+    if operation == 1:
+        pass
+
+    if operation == 1:
+        pass
